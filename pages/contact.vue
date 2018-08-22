e620137e4193e27f94f76136689bba7cb84fe512
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-08-22 17:43:28 -0400

    replacing content on contact page with content pulled from CMS

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="page-container">
+    <!-- Hero -->
     <section class="hero">
       <div class="hero__mask hero__mask--lightblue">
         <img src="~/assets/images/covers/contact.jpg" alt="" class="hero__image">
         <div class="wrapper hero__wrapper">
           <div class="hero__content">
-            <h1>Contact</h1>
-            <p class="hero__description">Lorem ipsum placeholder text dolor fugit aut et, odipiti ommos arum lautassitat landaecte alitatur. Ducia volupta tempore pudiae. Lorem ipsum dolor sit amet.</p>
+            <h1>{{ header.headline }}</h1>
+            <p class="hero__description">{{ header.description }}</p>
           </div>
         </div>
       </div>
     </section>
-
+    <!-- Contact Methods -->
     <section class="lifted-section">
       <div class="wrapper">
         <div class="cards--condensed-on-mobile cards--flex-on-desktop">
@@ -19,28 +20,36 @@
             <img src="~/assets/images/icons/lotus-icon-green.png" alt="Lotus icon with green gradient" class="card__icon">
             <span class="card__divider"></span>
             <div class="card__content">
-              <h3 class="card__title m-t-25">Customer Service</h3>
-              <a href="tel:+18001234567" class="card__copy card__copy--large">800-123-4567</a>
-              <h3 class="card__title m-t-25">Business</h3>
-              <a href="tel:+18001234567" class="card__copy card__copy--large">800-123-4567</a>
+              <div
+                v-for="(phone, index) in methods.phones"
+                :key="index"
+              >
+                <h3 class="card__title m-t-25">{{ phone.title }}</h3>
+                <a :href="`tel:+${stripNumber(phone.number)}`" class="card__copy card__copy--large">{{ phone.number }}</a>
+              </div>
             </div>
           </div>
           <div class="card card--flex">
             <img src="~/assets/images/icons/lotus-icon-blue.png" alt="Lotus icon with blue gradient" class="card__icon">
             <span class="card__divider bg-darkblue"></span>
             <div class="card__content">
-              <h3 class="card__title color-darkblue m-t-25">Category</h3>
-              <a href="mailto:" class="card__copy card__copy--large" target="_blank">email@novaremedy.com</a>
-              <h3 class="card__title color-darkblue m-t-25">Category</h3>
-              <a href="mailto:" class="card__copy card__copy--large" target="_blank">email@novaremedy.com</a>
+              <div
+                v-for="(email, index) in methods.emails"
+                :key="index"
+              >
+                <h3 class="card__title m-t-25 color-darkblue">{{ email.title }}</h3>
+                <a :href="`mailto:${email.email}`" class="card__copy card__copy--large">{{ email.email }}</a>
+              </div>
             </div>
           </div>
           <div class="card card--flex">
             <img src="~/assets/images/icons/lotus-icon-green.png" alt="Lotus icon with green gradient" class="card__icon">
             <span class="card__divider"></span>
             <div class="card__content">
-              <h3 class="card__title m-t-25">Mailing</h3>
-              <p class="card__copy card__copy--large">12345 Main Street<br>Ann Arbor, MI 40234</p>
+              <h3 class="card__title m-t-25">{{ methods.address.title }}</h3>
+              <p class="card__copy card__copy--large">
+                {{ methods.address.street }}<br>
+                {{ methods.address.city }}, {{ methods.address.state }} {{ methods.address.zip }}</p>
             </div>
           </div>
         </div>
@@ -48,7 +57,7 @@
     </section>
 
     <section class="padded-section">
-      <h2 class="color-darkblue">Get in touch</h2>
+      <h2 class="color-darkblue">{{ form.headline }}</h2>
       <contact-form></contact-form>
     </section>
   </div>
@@ -58,11 +67,20 @@
 import ContactForm from '~/components/ContactForm.vue';
 
 export default {
+  async asyncData({ params }) {
+    const pageData = await import('~/content/_contact.json');
+    return pageData;
+  },
   data() {
     return {};
   },
   components: {
     'contact-form': ContactForm,
+  },
+  methods: {
+    stripNumber(number) {
+      return number.replace(/\D/g, '');
+    },
   },
   head() {
     return {
